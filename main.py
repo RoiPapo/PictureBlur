fe78d50ff72321b44416438021ec16f960449f31
Roi <thedarkhermit@gmail.com>
2021-05-05 13:27:44 +0300

    finite

--- main.py
+++ main.py
@@ -1,8 +1,47 @@
+import time
+
 import numpy as np
 import scipy as scipy
 import matplotlib.pyplot as plt
 from scipy.linalg import toeplitz
 from scipy.sparse import csr_matrix, kron
+from scipy.sparse.linalg import eigs
+
+
+def const_step(s):
+    def lsearch(f, x, gf):
+        return s
+
+    return lsearch
+
+
+def exact_quad(A):
+    np.linalg.cholesky(A.toarray())
+
+    def lsearch(f, x, gf):
+        a = (np.linalg.norm(gf(x)) / (np.sqrt(2) * np.linalg.norm(A.toarray().dot(gf(x))))) ** 2
+        return a
+
+    return lsearch
+
+
+def gradient_method(f, gf, lsearch, x0, iterations):
+    x = x0
+    fs = []
+    gs = []
+    ts = []
+    gval = gf(x)
+    startime = time.time()
+    ts.append(0)
+    for i in range(iterations):
+        t = lsearch(f, x, gf)
+        x = x - t * gval
+        print('iter= {:2d} f(x)={:10.10f}'.format(i, f(x)))
+        gval = gf(x)
+        fs.append(f(x))
+        gs.append(np.linalg.norm(gval))
+        ts.append(time.time() - startime)
+    return x, fs, gs, ts
 
 
 def blur(N, band=3, sigma=0.7):
@@ -56,13 +95,67 @@ def blur(N, band=3, sigma=0.7):
     return A, b, x
 
 
+def fista(f, g, L, x0, eps, j):
+    y = x0
+    x_k = y
+    x_k_m1 = y
+    t_k = 1
+    t_k_1 = 0
+    fs = []
+    gs = []
+    ts = []
+    # gval = g(y)
+    gs.append(np.linalg.norm(g(y)))
+    i = 1
+    startime = time.time()
+    ts.append(0)
+    while i < j and gs[-1] > eps:
+        x_k = y - (1 / L) * g(y)
+        print('iter= {:2d} f(x)={:10.10f}'.format(i, f(x_k)))
+        t_k_1 = (1 + np.sqrt(1 + 4 * t_k ** 2)) / 2
+        y = x_k + ((t_k - 1) / t_k_1) * (x_k - x_k_m1)
+        x_k_m1 = x_k
+        t_k=t_k_1
+        fs.append(f(x_k))
+        gs.append(np.linalg.norm(g(x_k)))
+        i += 1
+        ts.append(time.time() - startime)
+    return x_k, fs, gs, ts
+
+
 def main():
-    A, b, x = blur(256, 5, 1)
-    plt.figure(figsize=(6, 6))
-    plt.imshow(x.reshape(256, 256), cmap='gray')
-    plt.show()
-    plt.figure(figsize=(6, 6))
-    plt.imshow(b.reshape(256, 256), cmap='gray')
+    A, b, x = blur(128, 5, 1)
+    # plt.figure(figsize=(6, 6))
+    # plt.imshow(x.reshape(256, 256), cmap='gray')
+    # plt.show()
+    # plt.figure(figsize=(6, 6))
+    # plt.imshow(b.reshape(256, 256), cmap='gray')
+    # plt.show()
+    f = lambda x: np.linalg.norm(A @ x - b) ** 2
+    g = lambda x: 2 * A.T @ (A @ x - b)
+    x0 = np.zeros([128 * 128, 1])
+    AtA = (A.T.dot(A)).toarray()
+    vals, vecs = eigs(AtA, k=1)
+
+    max_eign = np.real(vals[0])
+
+    # for j in [1, 10, 100, 1000]:
+    #     x_output1, f_s_output1, g_s_output1,ts_1 = gradient_method(f, g, exact_quad(A), x0, j)
+    #     x_output2, f_s_output2, g_s_output2,ts_2 = gradient_method(f, g, const_step(1 / (2 * max_eign)), x0, j)
+    #     x_fista, f_fista, g_fista, ts_fista = fista(f, g, 2 * max_eign, x0,10**-5, j)
+    #     plt.figure(figsize=(6, 6))
+    #     plt.imshow(x_fista.reshape(128, 128), cmap='gray')
+    #     plt.show()
+
+    # x_output1, f_s_output1, g_s_output1,ts_1 = gradient_method(f, g, exact_quad(A), x0, j)
+    x_output2, f_s_output2, g_s_output2, ts_2 = gradient_method(f, g, const_step(1 / (2 * max_eign)), x0, 1000)
+    x_fista, f_fista, g_fista, ts_fista = fista(f, g, 2 * max_eign, x0, 10 ** -5, 1000)
+    # plt.title('Gradient descend - gf value per second')
+    p1 = plt.semilogy(range(len(ts_2)), ts_2)
+    p2 = plt.semilogy(range(len(ts_fista)), ts_fista)
+    p3 = plt.semilogy(range(len(f_s_output2)), f_s_output2)
+    p4 = plt.semilogy(range(len(f_fista)), f_fista)
+    plt.legend((p1[0], p2[0], p3[0], p4[0]), ( 'time fista','time const 2', 'fs method2', 'fs fista'))
     plt.show()
 
 
